39ff2143003357f4bb12b40b1ac047ce28327a61
PandQ <78049382+PandQ-bot-code@users.noreply.github.com>
2025-05-18 07:40:49 +0200

    Update AboutDetails.tsx

--- src/pages/AboutDetails.tsx
+++ src/pages/AboutDetails.tsx
@@ -10,7 +10,7 @@ const AboutDetails = () => (
       development, I transform ideas into efficient, scalable solutions.
     </p>
 
-    {/* Education Section */}
+    {/* Edu*/}
     <div className="mb-12">
       <h2 className="text-2xl font-semibold text-konrad-blue mb-4">Education</h2>
       <ul className="list-disc pl-6 space-y-2">
@@ -21,7 +21,7 @@ const AboutDetails = () => (
       </ul>
     </div>
 
-    {/* Work Experience Section */}
+    {/* WExp */}
     <div>
       <h2 className="text-2xl font-semibold text-konrad-blue mb-4">Work Experience</h2>
       <ul className="list-disc pl-6 space-y-2">
